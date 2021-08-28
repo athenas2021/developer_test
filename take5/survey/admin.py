0943e2cc82444216924edbbc0fb1a450d0ede3a2
athenas2021 <lucas.kano@outlook.com>
2021-08-28 12:31:25 -0300

    aplicando correcoes models

--- take5/survey/admin.py
+++ take5/survey/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import Survey, SurveyQuestion, SurveyQuestionAlternative
+
+@admin.register(Survey)
+class SurveyAdmin(admin.ModelAdmin):
+    list_display = ("surveyText",)
+
+@admin.register(SurveyQuestion)
+class SurveyQuestionAdmin(admin.ModelAdmin):
+    list_display = ("questionText",)
+
+
+@admin.register(SurveyQuestionAlternative)
+class SurveyQuestionAlternativeAdmin(admin.ModelAdmin):
+    list_display = ("questionAlternativeText",)
+
 # Register your models here.
--- take5/survey/models.py
+++ take5/survey/models.py
@@ -1,23 +1,42 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-class Survey(models.Model):
-    title = models.CharField(max_length=255)
+
+
+
 
 class SurveyQuestion(models.Model):
-    survey  = models.ForeignKey(Survey, on_delete=models.CASCADE)
-    question = models.CharField(max_length=255)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # survey  = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    questionText = models.CharField(max_length=255)
+    questionAuthor = models.ForeignKey(User, on_delete=models.CASCADE)
+    # questionAlternatives = models.ManyToOneRel(SurveyQuestionAlternative)
+    # questionAlternatives = models.ManyToManyField(SurveyQuestionAlternative)
+    # questionAlternatives = models.ForeignKey(SurveyQuestionAlternative, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.questionText
 
-class SurveyQuestionAlternative(models.Model):
-    question= models.ForeignKey(SurveyQuestion, on_delete=models.CASCADE)
+class SurveyQuestionAlternative(models.Model):   
+    question = models.ForeignKey(SurveyQuestion, on_delete=models.CASCADE)
+    questionAlternativeText =  models.CharField(max_length=255)
     questionAlternative = models.CharField(max_length=1)
     rightAlternative = models.BooleanField()
+    def __str__(self):
+        return self.questionAlternativeText
+
+
+class Survey(models.Model):
+    surveyText = models.CharField(max_length=255)
+    surveyQuestions = models.ManyToManyField(SurveyQuestion)
+    def __str__(self):
+        return self.surveyText
+    
 
 class SurveyUserAnswer(models.Model):
-    questionAlternative = models.ForeignKey(SurveyQuestionAlternative, on_delete=models.CASCADE)
+    questionQuestion = models.ForeignKey(SurveyQuestion, on_delete=models.CASCADE)
     userAnswer = models.CharField(max_length=1)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.userAnswer
 
 
 # Create your models here.
