ba72f824a503a4c5977561ba301d3a50bc1768d8
athenas2021 <lucas.kano@outlook.com>
2021-08-28 10:19:44 -0300

    Alteraçao Models: adicionado campo: author (surveyquestion), campo: user (surveyuseranswer)

--- take5/survey/models.py
+++ take5/survey/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Survey(models.Model):
@@ -6,14 +7,17 @@ class Survey(models.Model):
 class SurveyQuestion(models.Model):
     survey  = models.ForeignKey(Survey, on_delete=models.CASCADE)
     question = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class SurveyQuestionAlternative(models.Model):
     question= models.ForeignKey(SurveyQuestion, on_delete=models.CASCADE)
     questionAlternative = models.CharField(max_length=1)
+    rightAlternative = models.BooleanField()
 
 class SurveyUserAnswer(models.Model):
     questionAlternative = models.ForeignKey(SurveyQuestionAlternative, on_delete=models.CASCADE)
     userAnswer = models.CharField(max_length=1)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 # Create your models here.
