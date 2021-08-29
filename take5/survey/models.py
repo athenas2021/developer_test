f9bf59ed1eed251a6aa4ac6c35dfbb1622afb781
athenas2021 <lucas.kano@outlook.com>
2021-08-29 12:21:48 -0300

    alteracao nas models, migrates realizados

--- take5/survey/models.py
+++ take5/survey/models.py
@@ -1,27 +1,42 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+class Survey(models.Model):
+    """
+    Model: Pesquisa
+    """    
+    surveyText = models.CharField(max_length=255)
+    surveyAuthor = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):              
+        return  self.surveyText
+
+
 class SurveyQuestion(models.Model):
-    questionText = models.CharField(max_length=255)
-    questionAuthor = models.ForeignKey(User, on_delete=models.CASCADE)    
+    """
+    Model: Questao
+    """
+    survey =  models.ForeignKey(Survey, on_delete=models.CASCADE)
+    questionText = models.CharField(max_length=255)        
     def __str__(self):
         return self.questionText
 
-class SurveyQuestionAlternative(models.Model):   
+
+class SurveyQuestionAlternative(models.Model):
+    """
+    Model: Alternativa
+    """   
     question = models.ForeignKey(SurveyQuestion, on_delete=models.CASCADE)
     questionAlternativeText =  models.CharField(max_length=255)
     questionAlternative = models.CharField(max_length=1)
     def __str__(self):
         return self.questionAlternativeText
-
-class Survey(models.Model):
-    surveyText = models.CharField(max_length=255)
-    surveyQuestions = models.ManyToManyField(SurveyQuestion)
-    def __str__(self):
-        return self.surveyText    
+   
 
 class SurveyUserAnswer(models.Model):
-    questionQuestion = models.ForeignKey(SurveyQuestion, on_delete=models.CASCADE)
+    """
+    Model: Resposta
+    """
+    question = models.ForeignKey(SurveyQuestion, on_delete=models.CASCADE)
     userAnswer = models.CharField(max_length=1)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
